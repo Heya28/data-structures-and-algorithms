1633ccbc4b31c5e7e8a06dce65dc5df178bd1cf8
Heya28 <hiyatrehan@gmail.com>
2025-08-22 01:57:09 +0530

    changes to check-if-sorted arrays easy

--- Leetcode/Striver_A2Z_Sheet/Arrays/Easy/check-if-sorted.cpp
+++ Leetcode/Striver_A2Z_Sheet/Arrays/Easy/check-if-sorted.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+// One Bonus at the end !
 /*
 EDGE CASES:
 SMALL:
@@ -13,33 +14,55 @@ SPECIAL:
 1. ALL ELEMENTS ARE SAME.
 2. DUPLICATES 
 
-Brute Force:
-TC: O(N^2)
+Optimal Approach:
+TC: O(N)
 SC: O(1)
-
 */
-bool is_sorted_array(vector<int> & arr){
+// bool is_sorted(vector<int>& arr){
+//     int size=arr.size();
+//     for(int i=0;i<size-1;i++){
+//         if(arr[i]>arr[i+1]){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// int main(){
+//     vector<int> array={};
+//     if(array.size()==0){
+//         cout<<"Empty array";
+//     }else{
+//         if(is_sorted(array)){
+//         cout<<"Array is sorted.";
+//     }else{
+//         cout<<"Array is not sorted.";
+//     }
+//     }
+    
+//     return 0;
+// }
+
+// BONUS
+// How would you modify your is_sorted function to check if an array is sorted in strictly increasing order 
+// (meaning no duplicates like {5, 5, 6} are allowed)?
+bool is_sorted(vector<int>& arr){
     int size=arr.size();
     for(int i=0;i<size-1;i++){
-        for(int j=i+1;j<size;j++){
-            if(arr[j]>arr[i]){
-                return false;
-            }
+        if(arr[i]>=arr[i+1]){
+            return false;
         }
     }
     return true;
 }
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    vector<int> array={-1,2,99,100};
-    bool result=is_sorted_array(array);
-    cout<<result;
-    if(array.size()!=0){
-        if(result){
-        cout<<"Array sorted.";
+    vector<int> array={};
+    if(array.size()==0){
+        cout<<"Empty array";
     }else{
-        cout<<"Not sorted.";
+        if(is_sorted(array)){
+        cout<<"Array is sorted.";
+    }else{
+        cout<<"Array is not sorted.";
     }
     }
     
--- Leetcode/Striver_A2Z_Sheet/Arrays/Easy/second-smallest.cpp
+++ Leetcode/Striver_A2Z_Sheet/Arrays/Easy/second-smallest.cpp
@@ -52,36 +52,123 @@ SC -> O(logn)
 // 2. Optimal Solution 
 // TC : O(N)
 // SC: O(1)
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     int minn=INT_MAX, secondminn =INT_MAX;
+//     int arr[]={7, 7, 2, 2, 10, 10, 10};
+//     int size=sizeof(arr)/sizeof(arr[0]);
+//     for(int i=0;i<size;i++){
+//         if(arr[i]<minn){
+//             secondminn=minn;
+//             minn=arr[i];
+//         }else if((arr[i]<secondminn)&& (arr[i]!=minn)){
+//             secondminn=arr[i];
+//         }
+//     }
+//     if(size>1){
+//         if(secondminn!=INT_MAX){
+//             cout<<"Second smallest element is "<<secondminn;
+//         }else{
+//             cout<<"Second smallest does not exist. All elements are identical.";
+//         }
+//     }else{
+//         if(size==0){
+//             cout<<"Empty array";
+//         }else{
+//             cout<<"Only one element in array. ";
+//         }
+        
+//     }
+//     return 0;
+// }
+
+// ### KEY IMPROVEMENTS 
+// NOTE: -> I am colliding data with the initialisation, what if INT_MAX was a number in the array? {INT_MAX,10}
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     int arr[]={INT_MAX,INT_MAX,INT_MAX};
+//     int size=sizeof(arr)/sizeof(arr[0]);
+//     int minn,secondminn;
+//     if(arr[0]<arr[1]){
+//         minn=arr[0];
+//         secondminn=arr[1];
+//     }else{
+//         minn=arr[1];
+//         secondminn=arr[0];
+//     }
+//     for(int i=2;i<size;i++){
+//         if(arr[i]<minn){
+//             secondminn=minn;
+//             minn=arr[i];
+//         }else if((arr[i]<secondminn) && (arr[i]!=minn)){
+//             secondminn=arr[i];
+//         }
+//     }
+//     if(size==0){
+//         cout<<"Empty array";
+//     }else if(size==1){
+//         cout<<"Only one element present";
+//     }else{
+//         if(secondminn!=minn){ // all identical
+//             cout<<"Second smallest is "<<secondminn;
+//         }else{
+//             cout<<"All elements are identical.";
+//         }
+//     }
+//     return 0;
+// }
+
+// 2. The above program WILL CRASH if there is an empty array or one element array !
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int minn=INT_MAX, secondminn =INT_MAX;
-    int arr[]={7, 7, 2, 2, 10, 10, 10};
+    int arr[]={};
     int size=sizeof(arr)/sizeof(arr[0]);
-    for(int i=0;i<size;i++){
+    int minn,secondminn;
+    if(size>1){
+    if(arr[0]<arr[1]){
+        minn=arr[0];
+        secondminn=arr[1];
+    }else{
+        minn=arr[1];
+        secondminn=arr[0];
+    }
+    for(int i=2;i<size;i++){
         if(arr[i]<minn){
             secondminn=minn;
             minn=arr[i];
-        }else if((arr[i]<secondminn)&& (arr[i]!=minn)){
+        }else if((arr[i]<secondminn) && (arr[i]!=minn)){
             secondminn=arr[i];
         }
     }
-    if(size>1){
-        if(secondminn!=INT_MAX){
-            cout<<"Second smallest element is "<<secondminn;
+    if(secondminn!=minn){ // all identical
+            cout<<"Second smallest is "<<secondminn;
         }else{
-            cout<<"Second smallest does not exist. All elements are identical.";
+            cout<<"All elements are identical.";
         }
+    }else if(size==0){
+        cout<<"Empty array";
     }else{
-        if(size==0){
-            cout<<"Empty array";
-        }else{
-            cout<<"Only one element in array. ";
-        }
-        
+        cout<<"Only one element present";
     }
     return 0;
 }
+
+/*Final Summary 
+1. Started with initialising INT_MIN and INT_MAX
+2. Edge case what if INT_MAX OR MIN is in array
+3. arr[0] and arr[1] initialising BUT by checking the values which one is greater or smaller
+4. Handling edge cases of an empty array and one element array 
+5. Handling Edge Cases of whether every element is the same. 
+*/
