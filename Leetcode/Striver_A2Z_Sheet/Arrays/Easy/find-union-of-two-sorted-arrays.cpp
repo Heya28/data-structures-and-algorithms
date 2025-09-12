bf9649822c06b88a3ccba979bfb8872c982804d8
Heya28 <hiyatrehan@gmail.com>
2025-09-12 11:42:40 +0530

    a

--- Leetcode/Striver_A2Z_Sheet/Arrays/Easy/find-union-of-two-sorted-arrays.cpp
+++ Leetcode/Striver_A2Z_Sheet/Arrays/Easy/find-union-of-two-sorted-arrays.cpp
@@ -6,37 +6,7 @@ using namespace std;
 // Optimal -> Two Pointer Approach ( SINCE Array is SORTED! )
 // vec.back() MUST be used if size >0 when size==0 crash and undefined behaviour.
 // unionarr.size() ==0 instead of i!=0 !!!!! ###### first element otherwise will not work!
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    vector<int> v1={1,1,2,3,4,5};
-    vector<int> v2={2,3,4,4,5};
-    vector<int> unionarr;
-    // if iteration in one of the arrays is over, then we will just go check for the second array for unique elements and if element in union array and append that.
-    int i=0,j=0;
-    while((i<v1.size())&&(j<v2.size())){
-        if(v1[i]<=v2[j]){ // equal case must also be considered.
-            if(unionarr.size()==0 || (unionarr.back()!=v1[i])){ // since array is sorted hence only need to check the last element.
-                unionarr.push_back(v1[i]);
-            }
-                            i++; // note where u are keeping this inside which if statement
-        }else if((v2[j]<v1[i])){
-            if(unionarr.size()==0 || unionarr.back()!=v2[j]){
-                unionarr.push_back(v2[j]);
-            }
-                            j++;
-        }
-    }
-    if(v1.size()<v2.size()){
-    for(int i=unionarr.size()-1;i<v2.size();i++){
-        
-    }
-    }
-    for(int i=0;i<unionarr.size();i++){
-        cout<<unionarr[i]<<' ';
-    }    
-    return 0;
-}
+
 
 // Brute Force - use sets ( DO NOT USE unsorted_set here as ordering will get distorted and you want the union is sorted order.)
 // TC is O(n1logN + n2logN) SC is O(n1+n2) +O(n1+n2) // strict upper bound worst case scenario if all are unique { for the union vector space}
